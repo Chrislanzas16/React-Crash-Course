7c3b8a55dc79e41a37db1c1d364b04947fde6b8a
Chris <chrislanzas89@outlook.com>
2025-07-28 19:50:45 -0400

    Worked on react useState and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,19 @@
 import "./App.css";
-import Todo from "./components/Todo.jsx";
-import Title from "./components/Title.jsx";
-import Modal from "./components/Modal.jsx";
-import React, { useState } from "react";
-import Counter from "./components/Counter.jsx";
+import { BrowserRouter as Router, Routes, Route,} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Nav from "./components/Nav.jsx"
+import Users from "./pages/Users";
 
 function App() {
-  const [showModal, setShowModal] = useState(false);
-
-  function onTodoDelete() {
-    setShowModal(true);
-    console.log("onTodoDelete");
-  }
-
-  function cancelModal() {
-    setShowModal(false);
-  }
-
-  function confirmModal() {
-    setShowModal(false);
-  }
-
   return (
-    <div>
-      <Title />
-      <div>
-        <input
-          type="text"
-          onChange={(event) => {
-            console.log(event.target.value);
-          }}
-        />
-        <button onClick={() => setShowModal(true)}>Add todo</button>
-      </div>
-      <div className="todo__wrapper">
-        <Todo onTodoDelete={onTodoDelete} title="Finish Frontend Simplified" />
-        <Todo onTodoDelete={onTodoDelete} title="Finish Interview Section" />
-        <Todo onTodoDelete={onTodoDelete} title="Land a 100K job" />
-      </div>
-      {showModal && (
-        <Modal
-          cancelModal={cancelModal}
-          confirmModal={confirmModal}
-          modal__title="Confirm delete?"
-        />
-      )}
-    </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/users/:id" element={<Users />} />
+        </Routes>
+      </Router>
   );
 }
 
